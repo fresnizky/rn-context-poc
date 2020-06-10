9f5e48551315b0f2a4e97e8b16a3eecdca4d118c
fresnizky <fresnizky@hotmail.com>
2020-06-10 11:02:21 -0300

    Added styles for Artist page

--- src/Item/Item.tsx
+++ src/Item/Item.tsx
@@ -3,6 +3,34 @@ import { ScrollView, Text, Image, View, FlatList } from 'react-native';
 import { useAlbumEffect } from '../state-mgmt/album/useAlbumEffect';
 import { GlobalContext } from '../state-mgmt/GlobalState';
 import { IAlbum } from 'src/state-mgmt/album/state';
+import styled from '@emotion/native';
+
+const Title = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+  line-height: 32px;
+  margin-bottom: 5px;
+`;
+
+const Content = styled.Text`
+  text-align: justify;
+  font-size: 16px;
+  line-height: 24px;
+`;
+
+const Container = styled.View`
+  padding: 10px;
+`;
+
+const AlbumContainer = styled.FlatList`
+  margin-right: -10px;
+`;
+
+const AlbumImage = styled.Image`
+  width: 75px;
+  height: 75px;
+  margin-right: 10px;
+`;
 
 export interface Props {
   route: any; // RouteProp<{ id: string; }, any>; /** @todo find out how to use this type */
@@ -23,11 +51,10 @@ const Item = ({ route }: Props) => {
       ),
     [state.album.albumMap]
   );
-  console.log(albumList);
 
   const { searchAlbumByArtistId } = useAlbumEffect();
   const keyExtractor = useCallback((item: IAlbum) => item.idAlbum, []);
-  const renderItem = useCallback(({ item }: { item: IAlbum }) => <Image style={{ width: 50, height: 50 }} source={{ uri: item.strAlbumThumb }} />, []);
+  const renderItem = useCallback(({ item }: { item: IAlbum }) => <AlbumImage source={{ uri: item.strAlbumThumb }} />, []);
 
   useEffect(() => {
     searchAlbumByArtistId(artist);
@@ -36,17 +63,17 @@ const Item = ({ route }: Props) => {
   return (
     <ScrollView contentInsetAdjustmentBehavior="automatic">
       <Image style={{ width: '100%', height: 100 }} source={{ uri: artist.strArtistBanner }} />
-      <View style={{ padding: 15 }}>
-        <Text>{artist.strArtist}</Text>
-        <Text ellipsizeMode="head" numberOfLines={30}>
-          {artist.strBiographyEN}
-        </Text>
-      </View>
+      <Container>
+        <Title>{artist.strArtist}</Title>
+        <Content ellipsizeMode="head" numberOfLines={30}>
+          {artist.strBiographyEN.replace('\n', '\n\n')}
+        </Content>
+      </Container>
       {albumList && (
-        <View>
-          <Text>Albums:</Text>
-          <FlatList horizontal data={albumList} keyExtractor={keyExtractor} renderItem={renderItem} />
-        </View>
+        <Container>
+          <Title>Albums:</Title>
+          <AlbumContainer horizontal data={albumList} keyExtractor={keyExtractor} renderItem={renderItem} />
+        </Container>
       )}
     </ScrollView>
   );
